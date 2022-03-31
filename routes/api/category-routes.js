5751dc4b28b703e25ffd041d7663159ea090b9a8
jacksonnorris <jacksonnorr@gmail.com>
2022-03-31 11:34:12 -0400

    added create and update functionality for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   try {
     const categoriesData = await Category.findByPk(req.params.id);
     if (!categoriesData) {
-      return res.status(404).json({message: 'no category found with this id'});
+      return res.status(404).json({message: 'There is no category found with this id'});
     }
     res.status(200).json(categoriesData);
   } catch (err) {
@@ -34,12 +34,23 @@ router.post('/', async (req, res) => {
     const categoriesData = await Category.create(req.body);
     res.status(200).json(categoriesData);
   } catch (err) {
-    res.status(400),json(err);
+    res.status(400).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoriesData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoriesData) { return res.status(404).json({message: 'There is no category found with this id'});}
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
