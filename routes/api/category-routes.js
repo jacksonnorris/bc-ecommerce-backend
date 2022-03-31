ffac696ba9f444d2c9a8bd8256190589fd82d428
jacksonnorris <jacksonnorr@gmail.com>
2022-03-31 11:23:20 -0400

    added routing for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,39 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+  try {
+    const categoriesData = await Category.findAll();
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoriesData = await Category.findByPk(req.params.id);
+    if (!categoriesData) {
+      return res.status(404).json({message: 'no category found with this id'});
+    }
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoriesData = await Category.create(req.body);
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(400),json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
